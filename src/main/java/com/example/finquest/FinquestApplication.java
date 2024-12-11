acd20a97fafca96e73977704cf96c70889574b51
momadAB <mabaqer99@gmail.com>
2024-12-11 10:43:34 +0300

    initial commit

---
+++ src/main/java/com/example/finquest/FinquestApplication.java
@@ -0,0 +1,13 @@
+package com.example.finquest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinquestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinquestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/finquest/FinquestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finquest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinquestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
