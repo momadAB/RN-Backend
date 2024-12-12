c9ac123e1eafdaa851e56baa34f23b8f9e02234a
f h <falhamad2002@gmail.com>
2024-12-12 11:42:38 +0300

    the get profile by id for parent and child is done

---
+++ src/main/java/com/example/finquest/bo/ChildUserResponse.java
@@ -0,0 +1,31 @@
+package com.example.finquest.bo;
+
+import com.example.finquest.entity.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChildUserResponse {
+
+    private Long id;
+    private Double balance;
+    private Long achievementPoints;
+    private String name;
+    private Long avatarId;
+    private String roles = "ROLE_CHILD";
+    private ParentUserEntity parentUser;
+    private List<Long> completedStepsOfRoadmap;
+    private List<RequestEntity> madeRequests;
+    private List<AchievementEntity> achievements;
+    private List<FriendEntity> friends;
+    private boolean isAllowedToMakeTransactionsWithNoPermission;
+
+    public ChildUserResponse(ChildUserEntity childUserEntity) {
+    }
+}
--- src/main/java/com/example/finquest/services/ChildUserService.java
+++ src/main/java/com/example/finquest/services/ChildUserService.java
@@ -1,4 +1,25 @@
 package com.example.finquest.services;
 
+import com.example.finquest.bo.ChildUserResponse;
+import com.example.finquest.bo.ParentUserResponse;
+import com.example.finquest.entity.ChildUserEntity;
+import com.example.finquest.entity.ParentUserEntity;
+import com.example.finquest.repository.ChildUserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class ChildUserService {
+
+    private final ChildUserRepository childUserRepository;
+
+    public ChildUserResponse getChildUserById(Long id) {
+        ChildUserEntity childUserEntity = childUserRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Child with ID " + id + " not found"));
+        ChildUserResponse childUserResponse = new ChildUserResponse(childUserEntity);
+        return childUserResponse;
+    }
+
+
 }
