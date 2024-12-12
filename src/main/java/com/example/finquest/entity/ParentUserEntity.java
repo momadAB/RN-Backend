4b8b311019a2c1212a34d7a2a96662d1bb670540
f h <falhamad2002@gmail.com>
2024-12-12 08:28:34 +0300

    added lombok to parent entity

--- src/main/java/com/example/finquest/entity/ParentUserEntity.java
+++ src/main/java/com/example/finquest/entity/ParentUserEntity.java
@@ -1,7 +1,16 @@
 package com.example.finquest.entity;
 import javax.persistence.*;
 import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class ParentUserEntity {
     @Id
@@ -20,16 +29,5 @@ public class ParentUserEntity {
     @OneToMany(mappedBy = "parentUser", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<ChildUserEntity> childUsers;
 
-    // Getters and setters
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
 
-    public String getUsername() { return username; }
-    public void setUsername(String username) { this.username = username; }
-
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
-
-    public String getRoles() { return roles; }
-    public void setRoles(String roles) { this.roles = roles; }
 }
