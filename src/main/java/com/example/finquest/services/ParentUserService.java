c408432ab4c5cefe56168eb3df14f125dfbd63c5
f h <falhamad2002@gmail.com>
2024-12-12 11:09:33 +0300

    changes

--- src/main/java/com/example/finquest/services/ParentUserService.java
+++ src/main/java/com/example/finquest/services/ParentUserService.java
@@ -1,4 +1,13 @@
 package com.example.finquest.services;
+import com.example.finquest.repository.ParentUserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class ParentUserService {
+
+    private final ParentUserRepository parentUserRepository;
+
+
 }
