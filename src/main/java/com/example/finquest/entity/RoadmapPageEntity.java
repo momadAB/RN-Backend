7b2593aa36af3d64f0e023d8e274ddcbdeb50d1f
momadAB <mabaqer99@gmail.com>
2024-12-18 14:30:01 +0300

    feat: add headers and text fields to RoadmapPageEntity

--- src/main/java/com/example/finquest/entity/RoadmapPageEntity.java
+++ src/main/java/com/example/finquest/entity/RoadmapPageEntity.java
@@ -32,6 +32,68 @@ public class RoadmapPageEntity {
     @JsonManagedReference
     private List<SelectableOptionEntity> options;
 
+    @Column
+    private String header1;
+    @Column
+    private String header2;
+    @Column
+    private String header3;
+
+    @Column
+    private String text1;
+    @Column
+    private String text2;
+    @Column
+    private String text3;
+
+    public String getHeader1() {
+        return header1;
+    }
+
+    public void setHeader1(String header1) {
+        this.header1 = header1;
+    }
+
+    public String getHeader2() {
+        return header2;
+    }
+
+    public void setHeader2(String header2) {
+        this.header2 = header2;
+    }
+
+    public String getHeader3() {
+        return header3;
+    }
+
+    public void setHeader3(String header3) {
+        this.header3 = header3;
+    }
+
+    public String getText1() {
+        return text1;
+    }
+
+    public void setText1(String text1) {
+        this.text1 = text1;
+    }
+
+    public String getText2() {
+        return text2;
+    }
+
+    public void setText2(String text2) {
+        this.text2 = text2;
+    }
+
+    public String getText3() {
+        return text3;
+    }
+
+    public void setText3(String text3) {
+        this.text3 = text3;
+    }
+
     public Long getId() {
         return id;
     }
